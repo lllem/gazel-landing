633a559e2f9d4fe6721bf61aa26a43d346e524e1
lllem <s.m.shemiakin@gmail.com>
2023-10-23 20:40:53 +0300

    Обновил футер

--- src/components/FooterEl.vue
+++ src/components/FooterEl.vue
@@ -2,42 +2,9 @@
   <footer class="bg-dark py-8 text-white">
     <div class="container max-w-screen-lg mx-auto px-4">
       <hr class="border-t mb-10 opacity-25">
-      <nav>
-        <ul>
-          <li class="text-cyan-400" v-for="route in routes" :key="route.path">
-            <router-link :to="route.path">{{ route.meta.title }}</router-link>
-          </li>
-        </ul>
+      <nav class="text-cyan-600">
+        ©&nbsp;«<router-link to="/" class="text-cyan-400">Вывоз мусора ГАЗелью</router-link>» 2023
       </nav>
     </div>
   </footer>
 </template>
-
-<script>
-// import HelloWorld from '@/components/HelloWorld.vue'
-
-export default {
-  components: {
-    // HelloWorld
-  },
-  data() {
-    return {
-      routes: [
-        {
-          path: '/',
-          meta: {
-            title: 'Главная',
-          },
-        },
-      ]
-    };
-  },
-}
-</script>
-
-
-<style lang="scss">
-.footer {
-  border: 1px solid black;
-}
-</style>
